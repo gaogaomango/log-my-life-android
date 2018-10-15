4372850be9c2229cb5d9cc88d86fe6c1a0f3ffe0
Ogai Masataka <gaogaomango@gmail.com>
2018-10-15 15:27:40 +1100

    make logout function

--- app/src/main/java/jp/co/mo/logmylife/main/MainActivity.java
+++ app/src/main/java/jp/co/mo/logmylife/main/MainActivity.java
@@ -1,15 +1,46 @@
 package jp.co.mo.logmylife.main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import jp.co.mo.logmylife.R;
+import jp.co.mo.logmylife.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        logoutButton = findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mAuth == null) {
+            mAuth = FirebaseAuth.getInstance();
+        }
     }
 }
